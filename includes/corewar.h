cc9f03ebe76577fe67c51d82a8979ce78fb061eb
mgross <marvin88gr@gmail.com>
2020-04-24 11:38:54 +0200

    Add header plus ifndef

--- includes/corewar.h
+++ includes/corewar.h
@@ -1,1 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   corewar.h                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: zitzak <zitzak@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/04/24 11:17:00 by zitzak        #+#    #+#                 */
+/*   Updated: 2020/04/24 11:19:19 by zitzak        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COREWAR_H
+# define COREWAR_H
+
+#include "libft.h"
+
+
 void	some_function(char *s);
+
+#endif
