2f00b4f7fea1adb5b7c0f7d8a91c173197a7ed38
mgross <marvin88gr@gmail.com>
2020-05-06 16:40:14 +0200

    Add asm to Makefile
    makefile now compiles both the vm and the asm if you call make

--- includes/asm.h
+++ includes/asm.h
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   corewar.h                                          :+:    :+:            */
+/*   asm.h                                              :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: zitzak <zitzak@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/24 11:17:00 by zitzak        #+#    #+#                 */
-/*   Updated: 2020/05/06 16:37:12 by zitzak        ########   odam.nl         */
+/*   Updated: 2020/05/06 16:36:07 by zitzak        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef COREWAR_H
-# define COREWAR_H
+#ifndef ASM_H
+# define ASM_H
 
 #include "libft.h"
 
 
-void	some_vm_function(char *s);
+void	some_asm_function(char *s);
 
 
 #endif
