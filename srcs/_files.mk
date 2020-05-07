e29a4df235fb818ee233635514fdb5aa7835859b
nstabel <nstabel@DESKTOP-K75P1TU.localdomain>
2020-05-07 18:31:04 +0200

    Add op files and t_op struct

--- srcs/_files.mk
+++ srcs/_files.mk
@@ -6,13 +6,14 @@
 #    By: mmarcell <mmarcell@student.codam.nl>         +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/02/28 18:56:22 by mmarcell      #+#    #+#                  #
-#    Updated: 2020/05/06 20:30:14 by nstabel       ########   odam.nl          #
+#    Updated: 2020/05/07 18:17:03 by nstabel       ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
 VM_FILES := some_vm_function.c
 
-ASM_FILES :=	asm_set_options.c 			\
+ASM_FILES :=	op.c						\
+				asm_set_options.c 			\
 				asm_get_input_file.c 		\
 				asm_tokenize_input.c 		\
 				asm_create_output_file.c 	\
