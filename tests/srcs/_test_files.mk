8e6b67e7fca11820ddd7783768f6084b72e159a1
mgross <marvin88gr@gmail.com>
2020-05-11 17:32:30 +0200

    Make criterion test files

--- tests/srcs/_test_files.mk
+++ tests/srcs/_test_files.mk
@@ -6,9 +6,9 @@
 #    By: mmarcell <mmarcell@student.codam.nl>         +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/02/28 18:56:18 by mmarcell      #+#    #+#                  #
-#    Updated: 2020/05/11 13:31:16 by zitzak        ########   odam.nl          #
+#    Updated: 2020/05/11 15:42:38 by zitzak        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
 TEST_FILES := 	test_some_function.c	\
-				test_lexical_analysis.s
+				test_get_input_file.s
