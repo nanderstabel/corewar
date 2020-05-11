8440899a421cc8244204c7d98c3fd2718406ed0a
mgross <marvin88gr@gmail.com>
2020-05-11 19:30:32 +0200

    Change test Makefile. Add test

--- tests/srcs/test_get_input_file.c
+++ tests/srcs/test_get_input_file.c
@@ -6,7 +6,7 @@
 /*   By: zitzak <zitzak@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/11 15:34:18 by zitzak        #+#    #+#                 */
-/*   Updated: 2020/05/11 15:54:30 by zitzak        ########   odam.nl         */
+/*   Updated: 2020/05/11 19:17:33 by zitzak        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,14 @@
 #include "libft.h"
 #include "asm.h"
 
-Test(test_get_input_file, no_extension)
+Test(test_get_input_file, tes)
 {
 	t_project	*as;
+	// t_bool		ret;
 
 	as = (t_project*)ft_memalloc(sizeof(t_project));
+	as->argv = (char**)malloc(sizeof(char*) * 1);
+	as->argv[0] = ft_strdup("42.s");
+	 get_input_file(as);
+	// cr_assert(ret == SUCCESS);
 }
