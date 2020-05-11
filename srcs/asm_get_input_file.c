e72a9160f5246b0d7460399e1e4d0916c571944a
mgross <marvin88gr@gmail.com>
2020-05-11 17:42:05 +0200

    First draft asm_get_input_files.c

--- srcs/asm_get_input_file.c
+++ srcs/asm_get_input_file.c
@@ -6,7 +6,7 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/06 19:27:58 by nstabel       #+#    #+#                 */
-/*   Updated: 2020/05/11 17:26:31 by zitzak        ########   odam.nl         */
+/*   Updated: 2020/05/11 17:33:11 by zitzak        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_bool			get_input_file(t_project *as)
 	as->count = (as->flags & DEBUG_O) ? ft_printf("%s\n", __func__) : 0;
 	temp = ft_strchr(*as->argv, '.');
 	if ((temp == NULL) | (!ft_strequ(temp, ".s")))
-		ft_printf("ERROR");
+		ft_printf("ERROR\n");
 	else
 	{
 		ft_printf("argc :%i\n argv %s\n", as->argc, *as->argv);
