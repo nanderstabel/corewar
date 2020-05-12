30ad6cea2dba9e949369ea0abae7367d8d817441
mgross <marvin88gr@gmail.com>
2020-05-12 18:22:50 +0200

    Change test_main

--- tests/srcs/test_main.c
+++ tests/srcs/test_main.c
@@ -6,7 +6,7 @@
 /*   By: zitzak <zitzak@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/12 17:49:58 by zitzak        #+#    #+#                 */
-/*   Updated: 2020/05/12 18:09:15 by zitzak        ########   odam.nl         */
+/*   Updated: 2020/05/12 18:20:35 by zitzak        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 
 int		main(void)
 {
-	ft_printf("test\n");
+	ft_printf("testddf\n");
 	return (0);
 }
