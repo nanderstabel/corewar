c1b5c413ced97e15a984355c26254728fb91664d
mgross <marvin88gr@gmail.com>
2020-05-12 18:55:46 +0200

    Fix to ft_wspace

--- libft/srcs/ft_wspace_strequ.c
+++ libft/srcs/ft_wspace_strequ.c
@@ -6,7 +6,7 @@
 /*   By: zitzak <zitzak@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/12 17:10:29 by zitzak        #+#    #+#                 */
-/*   Updated: 2020/05/12 17:22:17 by zitzak        ########   odam.nl         */
+/*   Updated: 2020/05/12 18:53:56 by zitzak        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ int		ft_wspace_strequ(const char *s1, const char *s2)
 {
 	size_t		index;
 
+	index = 0;
 	if (!s1 || !s2)
 		return (0);
-	while (s1[index] != ' ' || s1[index] != '\f' || s1[index] != '\v' ||
-	s1[index] != '\n' || s1[index] != '\0')
+	while (s1[index] != ' ' && s1[index] != '\f' && s1[index] != '\v' &&
+	s1[index] != '\n' && s1[index] != '\0')
 	{
 		index++;
 	}
