77e64675db3cf028b2099fe9da738a232052f0c7
nstabel <nstabel@DESKTOP-K75P1TU.localdomain>
2020-05-12 17:22:00 +0200

    Replace token_tab and add string functionality

--- srcs/asm_analyze_syntax.c
+++ srcs/asm_analyze_syntax.c
@@ -6,69 +6,46 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/06 19:27:58 by nstabel       #+#    #+#                 */
-/*   Updated: 2020/05/12 16:14:45 by nstabel       ########   odam.nl         */
+/*   Updated: 2020/05/12 17:20:37 by nstabel       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
-
-typedef struct			s_token
-{
-	struct s_token		*next;
-	int					type;
-}						t_token;
-
-int		seq_tab[END + 1][END + 1] = 
-{
-	[INDIRECT_LABEL] = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0},
-	[LABEL] = {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-	[INSTRUCTION] = {1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0},
-	[REGISTER] = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0},
-	[SEPARATOR] = {1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0},
-	[DIRECT_LABEL] = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0},
-	[DIRECT] = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0},
-	[COMMAND_NAME] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
-	[COMMAND_COMMENT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
-	[STRING] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-	[ENDLINE] = {2, 1, 1, 2, 2, 2, 2, 1, 1, 0, 1, 0, 1},
-	[INDIRECT] = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0},
-	[END] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}	
-};
 
 static t_token		*temp_test(void)
 {
 	t_token		*list;
 
 	list = ft_memalloc(sizeof(t_token));
-	list->type = COMMAND_NAME;
+	list->token_type = COMMAND_NAME;
 	list->next = ft_memalloc(sizeof(t_token));
-	list->next->type = STRING;
+	list->next->token_type = STRING;
 	list->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->type = ENDLINE;
+	list->next->next->token_type = ENDLINE;
 	list->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->type = COMMAND_COMMENT;
+	list->next->next->next->token_type = COMMAND_COMMENT;
 	list->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->type = STRING;
+	list->next->next->next->next->token_type = STRING;
 	list->next->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->next->type = ENDLINE;
+	list->next->next->next->next->next->token_type = ENDLINE;
 	list->next->next->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->next->next->type = LABEL;
+	list->next->next->next->next->next->next->token_type = LABEL;
 	list->next->next->next->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->next->next->next->type = ENDLINE;
+	list->next->next->next->next->next->next->next->token_type = ENDLINE;
 	list->next->next->next->next->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->next->next->next->next->type = INSTRUCTION;
+	list->next->next->next->next->next->next->next->next->token_type = INSTRUCTION;
 	list->next->next->next->next->next->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->next->next->next->next->next->type = REGISTER;
+	list->next->next->next->next->next->next->next->next->next->token_type = REGISTER;
 	list->next->next->next->next->next->next->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->next->next->next->next->next->next->type = SEPARATOR;
+	list->next->next->next->next->next->next->next->next->next->next->token_type = INSTRUCTION; //SEPARATOR;
 	list->next->next->next->next->next->next->next->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->next->next->next->next->next->next->next->type = DIRECT;
+	list->next->next->next->next->next->next->next->next->next->next->next->token_type = DIRECT;
 	list->next->next->next->next->next->next->next->next->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->next->next->next->next->next->next->next->next->type = SEPARATOR;
+	list->next->next->next->next->next->next->next->next->next->next->next->next->token_type = SEPARATOR;
 	list->next->next->next->next->next->next->next->next->next->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->next->next->next->next->next->next->next->next->next->type = DIRECT_LABEL;
+	list->next->next->next->next->next->next->next->next->next->next->next->next->next->token_type = DIRECT_LABEL;
 	list->next->next->next->next->next->next->next->next->next->next->next->next->next->next = ft_memalloc(sizeof(t_token));
-	list->next->next->next->next->next->next->next->next->next->next->next->next->next->next->type = ENDLINE;
+	list->next->next->next->next->next->next->next->next->next->next->next->next->next->next->token_type = ENDLINE;
 	list->next->next->next->next->next->next->next->next->next->next->next->next->next->next->next = NULL;
 	return (list);
 }
@@ -78,16 +55,23 @@ t_bool			analyze_syntax(t_project *as)
 	t_token		*list;
 
 	as->count = (as->flags & DEBUG_O) ? ft_printf("%s\n", __func__) : 0;
-	list = temp_test();
-	while (list->next)
+	if (as->flags & DEBUG_O)
 	{
-		if (seq_tab[list->type][list->next->type])
-			ft_printf("%i --> %i\n", list->type, list->next->type);
-		else
-			ft_printf("ERROR\n");
-		list = list->next;
+		ft_putchar(10);
+		list = temp_test();
+		while (list->next)
+		{
+			if (token_tab[list->token_type].next[list->next->token_type])
+				ft_printf("\t%s --> %s\n", token_tab[list->token_type].string, token_tab[list->next->token_type].string);
+			else
+			{
+				ft_printf("\tSyntax error at token [TOKEN][000:000] %s \"\"\n", token_tab[list->next->token_type].string);
+				break;
+			}
+			list = list->next;
+		}
+		ft_putchar(10);
 	}
-	//op_tab;
 	return (SUCCESS);	
 }
 
