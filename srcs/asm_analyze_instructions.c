16f823b0dbf9b524b67f36030e38f583743e6a9c
nstabel <nstabel@DESKTOP-K75P1TU.localdomain>
2020-05-15 04:11:16 +0200

    Add improved instruction, syntax, translation and parameter analysis

--- srcs/asm_analyze_instructions.c
+++ srcs/asm_analyze_instructions.c
@@ -6,7 +6,7 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/11 16:07:54 by nstabel       #+#    #+#                 */
-/*   Updated: 2020/05/13 03:18:19 by nstabel       ########   odam.nl         */
+/*   Updated: 2020/05/15 04:03:44 by nstabel       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,71 @@
 
 /*
 ** -------------------------------------------------------------------------- **
-** checks if the current token is of type INSTRUCTION. If this is the case,
-** then it will loop through the op_tab table to check if the instruction
-** is valid. If not, then an error message is printed and FAIL is returned.
-** If the instruction is valid, then the opcode is saved as well as the
-** information of the encoding byte (not the actual byte, just if one is needed
-** later on in the translation to byte-code). In this case SUCCESS is returned.
-** It also increments as->n_labels whenever it encounters a LABEL.
+** this function tries to find the current INSTRUCTION. It loops through the
+** operations stored in op_tab. If it finds the instruction, it's opcode gets
+** updated, and the encoding variable is switched on (later used to calculate
+** the encoding byte of the INSTRUCTION). SUCCESS is returned. If the
+** INSTRUCTION is not found, FAIL is returned.
 ** params
 **	as->current_token	the current token
-**	op_tab				operation table
+**	op	_tab			operation table
+** return
+**	SUCCESS				in case INSTRUCTION is found
+**	FAIL				in case INSTRUCTION is not found
+*/
+
+t_bool			find_instruction(t_project *as)
+{
+	as->index = 0;
+	while (as->index < REG_NUMBER)
+	{
+		if (ft_strequ(op_tab[as->index].operation, \
+			as->current_token->literal_str))
+		{
+			as->current_token->opcode = op_tab[as->index].opcode;
+			as->current_token->encoding = op_tab[as->index].encoded;
+			return (SUCCESS);
+		}
+		++as->index;
+	}
+	return (FAIL);
+}
+
+/*
+** -------------------------------------------------------------------------- **
+** if during the instruction analysis an invalid sequence is found, this
+** function prints the corresponding error message. It prints the generic
+** INSTRUCTION_ERR with ERROR_FORMAT.
+** params
+**	as->current_token	the current token
+**	token_tab			table with valid sequences
+** return
+**	FAIL				in case the sequence is invalid
+*/
+
+t_bool			instruction_error(t_project *as)
+{
+	ft_printf(INSTRUCTION_ERR);
+	ft_printf(ERROR_FORMAT, as->current_token->row + 1, \
+		as->current_token->column + 1, \
+		token_tab[as->current_token->token_type].string, \
+		as->current_token->literal_str);
+	return (FAIL);
+}
+
+/*
+** -------------------------------------------------------------------------- **
+** checks if the current token is of type INSTRUCTION. If this is the case,
+** then it will check if find_instruction can find the INSTRUCTION. If it can,
+** SUCCESS is returned, if not instruction_error is returned. If the current
+** token type is a label, then n_labels is incremented (later used to initialize
+** a label hashtable).
+** params
+**	as->current_token	the current token
 **	as->n_labels		number of labels (later used to malloc a hashtable)
 ** return
-**	SUCCESS		in case no invalid INSTRUCTION is found
-**	FAIL		in case the INSTRUCTION is invalid
+**	SUCCESS					in case no invalid INSTRUCTION is found
+**	instruction_error(as)	in case an error message is printed (== FAIL)
 */
 
 t_bool			instruction_check(t_project *as)
@@ -35,24 +86,9 @@ t_bool			instruction_check(t_project *as)
 	as->count = (as->flags & DEBUG_O) ? ft_printf("\t\t%s\n", __func__) : 0;
 	if (as->current_token->token_type == INSTRUCTION)
 	{
-		as->index = 0;
-		while (as->index < REG_NUMBER)
-		{
-			if (ft_strequ(op_tab[as->index].operation, \
-				as->current_token->literal_str))
-			{
-				as->current_token->opcode = op_tab[as->index].opcode;
-				as->current_token->encoding = op_tab[as->index].encoded;
-				return (SUCCESS);
-			}
-			++as->index;
-		}
-		ft_printf(INSTRUCTION_ERR);
-		ft_printf(ERROR_FORMAT, as->current_token->row, \
-			as->current_token->column, \
-			token_tab[as->current_token->token_type].string, \
-			as->current_token->literal_str);
-		return (FAIL);
+		if (find_instruction(as) == SUCCESS)
+			return (SUCCESS);
+		return (instruction_error(as));
 	}
 	else if (as->current_token->token_type == LABEL)
 		++as->n_labels;
--- srcs/asm_loop_token_list.c
+++ srcs/asm_loop_token_list.c
@@ -6,11 +6,19 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/12 20:32:11 by nstabel       #+#    #+#                 */
-/*   Updated: 2020/05/14 20:40:28 by nstabel       ########   odam.nl         */
+/*   Updated: 2020/05/15 03:01:37 by nstabel       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
+
+t_bool			skip_node(t_project *as)
+{
+	as->tmp = as->trail;
+	//add del_token_node here
+	as->tmp->next = as->tmp->next->next;
+	return (SUCCESS);
+}
 
 t_bool			loop_token_list(t_project *as, t_bool (*check)(t_project *as))
 {
@@ -25,7 +33,11 @@ t_bool			loop_token_list(t_project *as, t_bool (*check)(t_project *as))
 		as->current_token = (t_token *)as->tmp->content;
 		as->next_token = (t_token *)as->tmp->next->content;
 		if (check(as) == FAIL)
+		{
+			if (!as->tmp->next)
+				return (SUCCESS);
 			return (FAIL);
+		}
 		as->trail = as->tmp;
 		as->tmp = as->tmp->next;
 	}
