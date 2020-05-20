c81798cba95fd64ede2704f663d6d64e7909e972
moana <moanamarcello@gmail.com>
2020-05-20 15:45:32 +0200

    add '#define OP_H' to op.h

--- includes/op.h
+++ includes/op.h
@@ -13,11 +13,17 @@
 /*
 ** Toutes les tailles sont en octets.
 ** On part du principe qu'un int fait 32 bits. Est-ce vrai chez vous ?
+**
+** All sizes are in bytes.
+** We assume that an int is 32 bits. Is this true for you?
 */
 
-#define IND_SIZE			2
-#define REG_SIZE			4
-#define DIR_SIZE			REG_SIZE
+#ifndef OP_H
+# define OP_H
+
+# define IND_SIZE			2
+# define REG_SIZE			4
+# define DIR_SIZE			REG_SIZE
 
 
 # define REG_CODE			1
@@ -25,28 +31,28 @@
 # define IND_CODE			3
 
 
-#define MAX_ARGS_NUMBER		4
-#define MAX_PLAYERS			4
-#define MEM_SIZE			(4*1024)
-#define IDX_MOD				(MEM_SIZE / 8)
-#define CHAMP_MAX_SIZE		(MEM_SIZE / 6)
+# define MAX_ARGS_NUMBER	4
+# define MAX_PLAYERS		4
+# define MEM_SIZE			(4*1024)
+# define IDX_MOD			(MEM_SIZE / 8)
+# define CHAMP_MAX_SIZE		(MEM_SIZE / 6)
 
-#define COMMENT_CHAR		'#'
-#define LABEL_CHAR			':'
-#define DIRECT_CHAR			'%'
-#define SEPARATOR_CHAR		','
+# define COMMENT_CHAR		'#'
+# define LABEL_CHAR			':'
+# define DIRECT_CHAR		'%'
+# define SEPARATOR_CHAR		','
 
-#define LABEL_CHARS			"abcdefghijklmnopqrstuvwxyz_0123456789"
+# define LABEL_CHARS		"abcdefghijklmnopqrstuvwxyz_0123456789"
 
-#define NAME_CMD_STRING		".name"
-#define COMMENT_CMD_STRING	".comment"
+# define NAME_CMD_STRING	".name"
+# define COMMENT_CMD_STRING	".comment"
 
-#define REG_NUMBER			16
+# define REG_NUMBER			16
 
-#define CYCLE_TO_DIE		1536
-#define CYCLE_DELTA			50
-#define NBR_LIVE			21
-#define MAX_CHECKS			10
+# define CYCLE_TO_DIE		1536
+# define CYCLE_DELTA		50
+# define NBR_LIVE			21
+# define MAX_CHECKS			10
 
 /*
 **
@@ -54,10 +60,10 @@
 
 typedef char		t_arg_type;
 
-#define T_REG				1
-#define T_DIR				2
-#define T_IND				4
-#define T_LAB				8
+# define T_REG				1
+# define T_DIR				2
+# define T_IND				4
+# define T_LAB				8
 
 /*
 **
@@ -88,3 +94,5 @@ typedef struct		s_op
 }					t_op;
 
 extern t_op			op_tab[17];
+
+#endif
