ea8b26d953b3656d0cb342fe652947a37026fe7e
Lotte Hageman <lottehageman@MacBook-Pro-van-Lotte.local>
2020-05-25 19:38:24 +0200

    first draft arena test

--- graphics/corewargraphicdemo.c
+++ graphics/corewargraphicdemo.c
@@ -6,12 +6,14 @@
 /*   By: lhageman <lhageman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/17 12:20:07 by lhageman      #+#    #+#                 */
-/*   Updated: 2020/05/20 16:24:43 by lhageman      ########   odam.nl         */
+/*   Updated: 2020/05/25 19:35:39 by lhageman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "ft_graphics.h"
+#include "../includes/corewar.h"
+#include "../includes/op.h"
 
 void	ft_set_pairs()
 {
@@ -27,14 +29,8 @@ void	ft_set_pairs()
 	init_pair(9, ANSI_GREY, COLOR_BLACK);
 }
 
-int	main(int argc, char *argv[])
+void	ft_test_print(void)
 {
-	initscr();									//initialize the window
-	noecho();									//don't echo any keypresses
-	curs_set(FALSE);							//dont display a cursor
-	ft_set_pairs();
-	attron(COLOR_PAIR(9));
-	mvaddstr(0, 0, "This is a demo for Corewar!\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n");
 	while (1)
 	{
 		refresh();									//keeps updating your window
@@ -82,6 +78,92 @@ int	main(int argc, char *argv[])
 		mvaddstr(0, 0, "This is a demo for Corewar!\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n");
 		sleep(2);
 	}
+}
+
+void draw_borders(WINDOW *screen) 
+{ 
+	int x, y, i; getmaxyx(screen, y, x); 
+	// 4 corners 
+	// mvwprintw(screen, 0, 0, "+"); 
+	// mvwprintw(screen, y - 1, 0, "+"); 
+	// mvwprintw(screen, 0, x - 1, "+"); 
+	// mvwprintw(screen, y - 1, x - 1, "+"); 
+	// sides 
+	// for (i = 1; i < (y - 1); i++) 
+	// { 
+	// 	mvwprintw(screen, i, 0, "|"); 
+	// 	mvwprintw(screen, i, x - 1, "|"); 
+	// } // top and bottom 
+	for (i = 1; i < (x - 1); i++) 
+	{ 
+		mvwprintw(screen, 0, i, "-"); 
+		// mvwprintw(screen, y - 1, i, "-"); 
+	} 
+}
+
+int	ft_close_windows(WINDOW *arena, WINDOW *data)
+{
+	delwin(arena);
+	delwin(data);
+	return (0);
+}
+
+int	ft_print_data(WINDOW *data, t_vm *vm)
+{
+	char *str;
+
+	werase(data);
+	draw_borders(data);
+	mvwprintw(data, 1, 1, "CYLE:");
+	str = ft_itoa(vm->cycle_count);
+	mvwprintw(data, 1, 16, str);
+	ft_strdel(&str);
+	mvwprintw(data, 2, 1, "CYCLE_TO_DIE:");
+	str = ft_itoa(vm->CTD);
+	mvwprintw(data, 2, 16, str);
+	ft_strdel(&str);
+	wrefresh(data);
+	return (0);
+}
+
+int	ft_initiate_arena(t_vm *vm)
+{
+	int		parent_x;
+	int		parent_y;
+	int		data_size;
+	WINDOW	*arena;
+	WINDOW	*data;
+
+	data_size = 5;
+	getmaxyx(stdscr, parent_y, parent_x);
+	arena = newwin(parent_y - data_size, parent_x, 0, 0);
+	data = newwin(data_size, parent_x, parent_y - data_size, 0);
+	draw_borders(data);
+	sleep(2);
+	mvwprintw(arena, 1, 1, "I AM THE ARENA");
+	mvwprintw(data, 1, 1, "I AM DATA");
+	wrefresh(arena);
+	wrefresh(data);
+	sleep(2);
+	ft_print_data(data, vm);
+	sleep(10);
+	ft_close_windows(arena, data);
+	return (0);
+}
+
+int	main(void)
+{
+	initscr();									//initialize the window
+	noecho();									//don't echo any keypresses
+	curs_set(FALSE);							//dont display a cursor
+	ft_set_pairs();
+	// attron(COLOR_PAIR(9));
+	// mvaddstr(0, 0, "This is a demo for Corewar!\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00\n");
+	// ft_test_print();
+	t_vm vm;
+	vm.cycle_count = 0;
+	vm.CTD = CYCLE_TO_DIE;
+	ft_initiate_arena(&vm);
 	sleep(1);
 	endwin();
 }
---
+++ graphics/graphics.h
@@ -0,0 +1,9 @@
+#ifndef GRAPHICS_H
+# define GRAPHICS_H
+
+void	ft_attr_p1(int set);
+void	ft_attr_p2(int set);
+void	ft_attr_p3(int set);
+void	ft_attr_p4(int set);
+
+#endif
