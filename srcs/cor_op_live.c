0b8273841f09657620c313a7e9c1e9d1139feeff
moana <moanamarcello@gmail.com>
2020-06-02 13:27:20 +0200

    correct return value and add error handling

--- srcs/cor_op_live.c
+++ srcs/cor_op_live.c
@@ -18,13 +18,14 @@ int		op_live(t_vm *vm, t_cursor *cursor)
 	int				player_number;
 
 	arg_position = new_idx(cursor->pc, 1, FALSE);
-	vm->last_live = convert_to_int(arg_position, 4);
-	player_number = -(vm->last_live);
+	if (0 < player_number && player_number <= MAX_PLAYERS && \
+		vm->champions[player_number != NULL])
+	{
+		vm->last_live = convert_to_int(arg_position, 4);
+		player_number = -(vm->last_live);
+		ft_printf("A process shows that player %d (%s) is alive\n", \
+			player_number, vm->champions[player_number]->prog_name);
+	}
 	//call visualizer
-	if (!(0 < player_number && player_number <= MAX_PLAYERS && \
-		vm->champions[player_number != NULL]))
-		return (ERROR);
-	ft_printf("A process shows that player %d (%s) is alive\n", \
-		player_number, vm->champions[player_number]->prog_name);
-	return (SUCCESS);
+	return (5);
 }
