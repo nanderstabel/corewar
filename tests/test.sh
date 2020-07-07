26d90331435c8231770e5f5e74259a4e056117e0
nstabel <nstabel@DESKTOP-K75P1TU.localdomain>
2020-07-07 11:18:00 +0200

    Add test.sh

---
+++ tests/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+for file in $@; do
+	echo $file
+	./vm-champ-linux/asm $file
+	file=`echo $file | sed 's/.s$//g'`
+	mv "${file}.cor" "${file}_2.cor"
+	../asm "${file}.s"
+	sleep 1
+	diff "${file}.cor" "${file}_2.cor"
+done
