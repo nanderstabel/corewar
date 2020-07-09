7b15898cf78a3d5e68cb9996b4003a6c0ad53872
mgross <marvin88gr@gmail.com>
2020-07-09 18:15:26 +0200

    asm_free_project norm

--- srcs/asm_free_project.c
+++ srcs/asm_free_project.c
@@ -6,7 +6,7 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/15 20:44:21 by nstabel       #+#    #+#                 */
-/*   Updated: 2020/05/22 16:57:58 by nstabel       ########   odam.nl         */
+/*   Updated: 2020/07/09 18:14:40 by zitzak        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@ t_bool			free_project(t_project *as)
 {
 	as->count = (as->flags & DEBUG_O) ? ft_printf("%s\n", __func__) : 0;
 	ft_free_hash_table(&as->labels, NULL);
-	// while (as->token_list)
-	// {
-	// 	ft_printf("token: %s, %i\n", token_tab[((t_token *)as->token_list->content)->token_type].string, ((t_token *)as->token_list->content)->token_type);
-	// 	as->token_list = as->token_list->next;
-	// }
 	as->tmp = as->token_list->next;
 	as->trail = as->token_list;
 	del_token_list(as);
