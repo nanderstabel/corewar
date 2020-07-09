dcfa47194aaba45659a813a42beacb25d4ef1777
mgross <marvin88gr@gmail.com>
2020-07-09 18:50:26 +0200

    deleted tokenize_input

--- srcs/_files.mk
+++ srcs/_files.mk
@@ -6,7 +6,7 @@
 #    By: mmarcell <mmarcell@student.codam.nl>         +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/02/28 18:56:22 by mmarcell      #+#    #+#                  #
-#    Updated: 2020/07/09 18:44:57 by zitzak        ########   odam.nl          #
+#    Updated: 2020/07/09 18:48:42 by zitzak        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,6 @@ ASM_FILES :=	op.c						\
 				asm_set_options.c 			\
 				asm_get_input_file.c 		\
 				asm_analyze_lexicon.c 		\
-				asm_tokenize_input.c 		\
 				asm_analyze_syntax.c 		\
 				asm_analyze_parameters.c 	\
 				asm_analyze_instructions.c 	\
