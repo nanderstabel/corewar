0f125babd00d22d32fa3e42e3ea9a6f627d224ce
nstabel <nstabel@DESKTOP-K75P1TU.localdomain>
2020-07-09 16:44:29 +0200

    Add exit() to usage_message and get_input_file

--- srcs/asm_set_options.c
+++ srcs/asm_set_options.c
@@ -6,7 +6,7 @@
 /*   By: nstabel <nstabel@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/06 19:27:58 by nstabel       #+#    #+#                 */
-/*   Updated: 2020/05/18 12:20:54 by zitzak        ########   odam.nl         */
+/*   Updated: 2020/07/09 16:40:33 by nstabel       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,11 +75,11 @@ t_bool				print_usage_message(t_project *as)
 {
 	as->count = (as->flags & DEBUG_O) ? ft_printf("\t%s\n", __func__) : 0;
 	ft_printf("Usage: ./asm [-%s] <sourcefile.s>\n", OPTIONS);
-	ft_printf("\t-a : Instead of creating a .cor file, outputs a stripped and\
-		annotated version of the code to the standard output\n");
+	//ft_printf("\t-a : Instead of creating a .cor file, outputs a stripped and annotated version of the code to the standard output\n");
 	ft_printf(\
 	"\t-g : Debug mode: shows executed functions in chronological order\n");
 	ft_printf("\t-u : Shows usage message\n");
+	exit(0);
 	return (SUCCESS);
 }
 
