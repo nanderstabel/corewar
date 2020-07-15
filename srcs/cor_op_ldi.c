ae1a6824e8d7bde7302ed06b59413b16cbea93cc
nstabel <nstabel@DESKTOP-K75P1TU.localdomain>
2020-07-15 09:39:37 +0200

    Commit ldi

--- srcs/cor_op_ldi.c
+++ srcs/cor_op_ldi.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   cor_op_ldi.c                                       :+:    :+:            */
+/*   cor_op_ldii.c                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mmarcell <mmarcell@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
@@ -12,10 +12,59 @@
 
 #include "corewar.h"
 
-void	op_ldi(t_vm *vm, t_cursor *cursor)
+// If Argument2 is of type T_REG, set value from registry <Argument1> into registry <Argument2>
+// If Argument2 is of type T_IND, set value from registry <Argument1> into memory. 
+// Address is calculated as follows: (current_position + (Argument2 % IDX_MOD))
+
+// REG 01	1	Registry Rx (where х = registry number, 1 to REG_NUMBER)			
+// DIR 10	2-4	A number, saved on 2 or 4 bytes, depending on label			
+// IND 11	2	Relative address, read 4 bytes from position <±T_IND bytes>
+
+// 01010000
+static int	op_ldi_check(t_vm *vm, t_cursor *cursor)
 {
-	if (vm == NULL || cursor == NULL)
+	unsigned char	enc;
+
+	enc = vm->arena[new_idx(cursor->pc, 1, 0)];
+	if (get_arg_type(enc, 4) != 0 || \
+		get_arg_type(enc, 3) != 0 || \
+		get_arg_type(enc, 2) != REG || \
+		get_arg_type(enc, 1) == 0 ||
+		get_arg_type(enc, 1) == REG)
+	{
+		cursor->pc = new_idx(cursor->pc, 1, FALSE);
+		return (ERROR);
+	}
+	else
+		return (SUCCESS);
+}
+
+void		op_ldi(t_vm *vm, t_cursor *cursor)
+{
+	int		arg_1;
+	int		arg_2;
+	int		type;
+
+	type = get_arg_type(vm->arena[new_idx(cursor->pc, 1, FALSE)], 1);
+	if (op_ldi_check(vm, cursor) != SUCCESS)
 		return ;
+	if (type == DIR)
+	{
+		arg_1 = convert_to_int(vm->arena, new_idx(cursor->pc, 2, 0), 4);
+		arg_2 = convert_to_int(vm->arena, new_idx(cursor->pc, 6, 0), 1);
+		if (arg_2 < REG_NUMBER)
+			cursor->reg[arg_2] = arg_1;
+		cursor->pc = new_idx(cursor->pc, 7, FALSE);
+	}
+	else if (type == IND)
+	{
+		arg_1 = convert_to_int(vm->arena, new_idx(cursor->pc, 2, 0), 2);
+		arg_1 = convert_to_int(vm->arena, new_idx(cursor->pc, arg_1, 0), 4);
+		arg_2 = convert_to_int(vm->arena, new_idx(cursor->pc, 4, 0), 1);
+		if (arg_2 < REG_NUMBER)
+			cursor->reg[arg_2] = arg_1;
+		cursor->pc = new_idx(cursor->pc, 5, FALSE);
+	}
 }
 
 
