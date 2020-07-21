971d4653d639e3b9258678acd5488979e7b1093d
Lotte Hageman <lottehageman@MacBook-Pro-van-Lotte.local>
2020-07-21 12:04:16 +0200

    extra color set for live reporting

--- srcs/vis_attr_players.c
+++ srcs/vis_attr_players.c
@@ -6,7 +6,7 @@
 /*   By: lhageman <lhageman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/20 16:24:05 by lhageman      #+#    #+#                 */
-/*   Updated: 2020/06/11 09:50:15 by lhageman      ########   odam.nl         */
+/*   Updated: 2020/07/21 11:50:35 by lhageman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ void	vis_set_pairs(void)
 	start_color();
 	init_pair(1, ANSI_COPPER, COLOR_BLACK);
 	init_pair(2, COLOR_BLACK, ANSI_COPPER);
+	init_pair(11, COLOR_WHITE, ANSI_COPPER);
 	init_pair(3, ANSI_ETON_BLUE, COLOR_BLACK);
 	init_pair(4, COLOR_BLACK, ANSI_ETON_BLUE);
+	init_pair(12, COLOR_WHITE, ANSI_ETON_BLUE);
 	init_pair(5, ANSI_OCEAN_GREEN, COLOR_BLACK);
 	init_pair(6, COLOR_BLACK, ANSI_OCEAN_GREEN);
+	init_pair(13, COLOR_WHITE, ANSI_OCEAN_GREEN);
 	init_pair(7, ANSI_WHEAT, COLOR_BLACK);
 	init_pair(8, COLOR_BLACK, ANSI_WHEAT);
+	init_pair(14, COLOR_WHITE, ANSI_WHEAT);
 	init_pair(9, ANSI_GREY, COLOR_BLACK);
 	init_pair(10, ANSI_PINK, ANSI_PINK);
 }
@@ -34,14 +38,13 @@ void	vis_attr_p1(int set, WINDOW *arena)
 		wattron(arena, COLOR_PAIR(1));
 		if (set == 2)
 			wattron(arena, A_BOLD);
-		else
-			wattroff(arena, A_BOLD);
 	}
 	if (set == 3)
-	{
 		wattron(arena, COLOR_PAIR(2));
+	if (set == 4)
+		wattron(arena, COLOR_PAIR(11));
+	if (set == 1 || set == 3 || set == 4)
 		wattroff(arena, A_BOLD);
-	}
 }
 
 void	vis_attr_p2(int set, WINDOW *arena)
@@ -51,14 +54,13 @@ void	vis_attr_p2(int set, WINDOW *arena)
 		wattron(arena, COLOR_PAIR(3));
 		if (set == 2)
 			wattron(arena, A_BOLD);
-		else
-			wattroff(arena, A_BOLD);
 	}
 	if (set == 3)
-	{
 		wattron(arena, COLOR_PAIR(4));
+	if (set == 4)
+		wattron(arena, COLOR_PAIR(12));
+	if (set == 1 || set == 3 || set == 4)
 		wattroff(arena, A_BOLD);
-	}
 }
 
 void	vis_attr_p3(int set, WINDOW *arena)
@@ -68,14 +70,13 @@ void	vis_attr_p3(int set, WINDOW *arena)
 		wattron(arena, COLOR_PAIR(5));
 		if (set == 2)
 			wattron(arena, A_BOLD);
-		else
-			wattroff(arena, A_BOLD);
 	}
 	if (set == 3)
-	{
 		wattron(arena, COLOR_PAIR(6));
+	if (set == 4)
+		wattron(arena, COLOR_PAIR(13));
+	if (set == 1 || set == 3 || set == 4)
 		wattroff(arena, A_BOLD);
-	}
 }
 
 void	vis_attr_p4(int set, WINDOW *arena)
@@ -85,12 +86,11 @@ void	vis_attr_p4(int set, WINDOW *arena)
 		wattron(arena, COLOR_PAIR(7));
 		if (set == 2)
 			wattron(arena, A_BOLD);
-		else
-			wattroff(arena, A_BOLD);
 	}
 	if (set == 3)
-	{
 		wattron(arena, COLOR_PAIR(8));
-		wattroff(arena, A_BOLD);
-	}
+	if (set == 4)
+		wattron(arena, COLOR_PAIR(14));
+	if (set == 1 || set == 3 || set == 4)
+		watroff(arena, A_BOLD);
 }
