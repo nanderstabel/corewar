297f7a2c387230d2ae0477be5412e65c58812352
nstabel <nstabel@DESKTOP-K75P1TU.localdomain>
2020-07-23 11:48:23 +0200

    Update tests

--- tests/archie_unittest/manual_diff_linux.sh
+++ tests/archie_unittest/manual_diff_linux.sh
@@ -4,10 +4,10 @@ OUR_COR="../../corewar"
 PATH_TO_PLAYER="../valid_asm/"
 PLAYER="vm_test"
 
-DUMP=5
+DUMP=0
 rm diff/og_output diff/our_output
 $OG_COR $PATH_TO_PLAYER$PLAYER.cor -d $DUMP > diff/og_output
 $OUR_COR $PATH_TO_PLAYER$PLAYER.cor -d $DUMP > diff/our_output
 if ! cmp -s "diff diff/og_output" "diff/our_output"; then
-    printf '-----no diff'
+    echo '-----no diff'
 fi
