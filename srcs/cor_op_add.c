6cc669a63059efa6aee1fd5250a39f639c6f5941
moana <moanamarcello@gmail.com>
2020-07-23 18:28:49 +0200

    alter op_add

--- srcs/cor_op_add.c
+++ srcs/cor_op_add.c
@@ -25,27 +25,27 @@ static int	op_add_check(t_vm *vm, t_cursor *cursor)
 		return (SUCCESS);
 }
 
-static int		get_value(t_vm *vm, t_cursor *cursor, int type, int *size)
-{
-	if (type == REG)
-	{
-		type = convert_to_int(vm->arena, new_idx(cursor->pc, *size, 0), 1);
-		type = cursor->reg[type];
-		*size += 1;
-	}
-	else if (type == IND)
-	{
-		type = convert_to_int(vm->arena, new_idx(cursor->pc, *size, 0), 2);
-		type = convert_to_int(vm->arena, new_idx(cursor->pc, type, 0), 4);
-		*size += 2;
-	}
-	else if (type == DIR)
-	{
-		type = convert_to_int(vm->arena, new_idx(cursor->pc, *size, 0), 4);
-		*size += 4;
-	}
-	return (type);
-}
+// static int		get_value(t_vm *vm, t_cursor *cursor, int type, int *size)
+// {
+// 	if (type == REG)
+// 	{
+// 		type = convert_to_int(vm->arena, new_idx(cursor->pc, *size, 0), 1);
+// 		type = cursor->reg[type];
+// 		*size += 1;
+// 	}
+// 	else if (type == IND)
+// 	{
+// 		type = convert_to_int(vm->arena, new_idx(cursor->pc, *size, 0), 2);
+// 		type = convert_to_int(vm->arena, new_idx(cursor->pc, type, 0), 4);
+// 		*size += 2;
+// 	}
+// 	else if (type == DIR)
+// 	{
+// 		type = convert_to_int(vm->arena, new_idx(cursor->pc, *size, 0), 4);
+// 		*size += 4;
+// 	}
+// 	return (type);
+// }
 
 int		op_add(t_vm *vm, t_cursor *cursor)
 {
@@ -53,21 +53,19 @@ int		op_add(t_vm *vm, t_cursor *cursor)
 	int		arg_1;
 	int		arg_2;
 	int		arg_3;
-	int		size;
 
 	if (op_add_check(vm, cursor) != SUCCESS)
 		return (ERROR);
-	size = 2;
-	arg_1 = get_value(vm, cursor, REG, &size);
-	arg_2 = get_value(vm, cursor, REG, &size);
-	arg_3 = convert_to_int(vm->arena, new_idx(cursor->pc, size, 0), 1);
-
-	if (arg_3 < REG_NUMBER)
-		cursor->reg[arg_3] = arg_1 + arg_2;
-	else
-		return (ERROR);
-	size++;
-	cursor->pc = new_idx(cursor->pc, size, FALSE);
-	cursor->carry = (cursor->reg[arg_3]) ? 0 : 1;
+	arg_1 = convert_to_int(vm->arena, new_idx(cursor->pc, 2, 0), 1);
+	arg_2 = convert_to_int(vm->arena, new_idx(cursor->pc, 3, 0), 1);
+	arg_3 = convert_to_int(vm->arena, new_idx(cursor->pc, 4, 0), 1);
+	if (0 < arg_1 && arg_1 <= REG_NUMBER && \
+		0 < arg_2 && arg_2 <= REG_NUMBER && \
+		0 < arg_3 && arg_3 <= REG_NUMBER)
+	{
+		cursor->reg[arg_3] = cursor->reg[arg_1] + cursor->reg[arg_2];
+		cursor->carry = (cursor->reg[arg_3]) ? 0 : 1;
+	}
+	cursor->pc = new_idx(cursor->pc, 5, FALSE);
 	return (SUCCESS);
 }
