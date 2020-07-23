78044a961ceb7c6a9fce4c2ff298795a3fc0dad6
Marvin <marvin88gr@gmail.com>
2020-07-23 17:19:37 +0200

    change to scripts

--- tests/archie_unittest/manual_diff_linux.sh
+++ tests/archie_unittest/manual_diff_linux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 OG_COR="cor_bin/lin/corewar"
 OUR_COR="../../corewar"
-PATH_TO_PLAYER="../valid_asm/"
-PLAYER="vm_test"
+PATH_TO_PLAYER="../valid_asm/valid_cor/"
+PLAYER="test_and3"
 
 if [ -z "$1" ]
   then
@@ -10,11 +10,15 @@ if [ -z "$1" ]
     else
     DUMP=$1
 fi
-echo $DUMP
 
+rm diff/og_output diff/our_output
 $OG_COR $PATH_TO_PLAYER$PLAYER.cor -d $DUMP > diff/og_output
 $OUR_COR $PATH_TO_PLAYER$PLAYER.cor -d $DUMP > diff/our_output
-if ! cmp -s "diff diff/og_output" "diff/our_output"; then
+DIFF=$(diff "diff/og_output" "diff/our_output")
+if [ "$DIFF" != "" ]
+then
+  diff "diff/og_output" "diff/our_output"
+
+else
     echo '-----no diff'
 fi
-rm diff/og_output diff/our_output
