2c9fadbb682a91382d55bceedb56b7eeb9614ab2
nstabel <n.g.stabel@hotmail.com>
2020-08-06 17:26:56 +0200

    Add champs

--- zorkeater.s
+++ zorkeater.s
@@ -1,28 +1,38 @@
-.name "zorkeater"
-.comment "I eat zork"
+.name "Peanutbutter"
+.comment "Don't mix"
 
 	ld %72399550, r2
 	st r2, :first
 
-four:
-    ld %4, r3
-
-base:
-    ld %200, r4
-    
 first:
 	xor %3133016936, r1, r1
-	zjmp %300
+	zjmp %3000
 
-loop:
-    live %-1
-    ld %1107727392, r6
-    sti r6, :base, %:four
-    add r4, r3, r4
+load:
+    ld %33554431, r2
+    ld %4278976576, r3
+    ld %42991616, r4
+    ld %269025351, r6
+    ld %64, r7
+    ld %64, r8
+    ld %384, r9
+
+store:
+    sti r2, %44, r7
+    sti r3, %42, r7
+    sti r4, %40, r7
+    sti r5, %38, r7
+    sti r6, %34, r7
+    sti r1, %15, r7
+    add r7, r8, r7
+    fork %:store
+
+jump:
     ld %0, r16
-    zjmp %:loop
+    zjmp %54
 
-repeat:
-    live %-1
-    add r3, r4, r5
-    zjmp %:repeat
+fork:
+#    live %-1
+ #   fork %64
+#    ld %0, r16
+ #   zjmp %:fork
