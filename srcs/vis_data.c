ac84595e7d0e70a474deb3fcfc9f94c8b181d38f
lhageman <lamhageman@gmail.com>
2020-08-07 10:18:31 +0200

    modified and added data, norm_proof

--- srcs/vis_data.c
+++ srcs/vis_data.c
@@ -6,7 +6,7 @@
 /*   By: lhageman <lhageman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/11 08:01:52 by lhageman      #+#    #+#                 */
-/*   Updated: 2020/08/01 14:42:54 by lhageman      ########   odam.nl         */
+/*   Updated: 2020/08/07 10:17:21 by lhageman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,28 @@ void	reset_color(t_vm *vm)
 	wattron(vm->vis->graphics->data, A_BOLD);
 }
 
-void	vis_print_players(t_vm *vm)
+void	vis_print_line(WINDOW *data, int y, int x, int nbr)
+{
+	char *str;
+
+	str = ft_itoa(nbr);
+	mvwprintw(data, y, x, "      ");
+	mvwprintw(data, y, x, str);
+	ft_strdel(&str);
+}
+
+void	vis_print_players(t_vm *vm, int y)
 {
 	unsigned int	i;
-	int				y;
 	char			*name;
 
 	i = 1;
-	y = 8;
 	while (i <= vm->champ_count)
 	{
 		vm->vis->attr[i](vis_calc_att(false, false), vm->vis->graphics->data);
+		mvwprintw(vm->vis->graphics->data, y + i, 4, "PLAYER ");
+		vis_print_line(vm->vis->graphics->data, y + i, 11, i);
+		y += 1;
 		name = ft_strsub(vm->champ[i]->header.prog_name, 0, 50);
 		mvwprintw(vm->vis->graphics->data, y + i, 4, name);
 		ft_strdel(&name);
@@ -61,28 +72,21 @@ void	vis_print_players(t_vm *vm)
 
 int		vis_print_data(t_vm *vm)
 {
-	char *str;
-
 	reset_color(vm);
-	mvwprintw(vm->vis->graphics->data, 3, 4, "CYCLE:");
-	mvwprintw(vm->vis->graphics->data, 3, 22, "     ");
-	str = ft_itoa(vm->total_cycle_count);
-	mvwprintw(vm->vis->graphics->data, 3, 22, str);
-	ft_strdel(&str);
-	mvwprintw(vm->vis->graphics->data, 4, 4, "CHECKS TOTAL");
-	str = ft_itoa(vm->check_count);
-	mvwprintw(vm->vis->graphics->data, 4, 22, str);
-	ft_strdel(&str);
-	mvwprintw(vm->vis->graphics->data, 5, 4, "CYCLES TO DIE");
-	str = ft_itoa(vm->ctd);
-	mvwprintw(vm->vis->graphics->data, 5, 22, str);
-	ft_strdel(&str);
-	mvwprintw(vm->vis->graphics->data, 6, 4, "LAST LIVE PLAYER:");
-	str = ft_itoa(vm->last_live);
+	mvwprintw(vm->vis->graphics->data, 3, 4, "TOTAL CYLES:");
+	vis_print_line(vm->vis->graphics->data, 3, 22, vm->total_cycle_count);
+	mvwprintw(vm->vis->graphics->data, 4, 4, "CYCLES TO DIE:");
+	vis_print_line(vm->vis->graphics->data, 4, 22, vm->ctd);
+	mvwprintw(vm->vis->graphics->data, 5, 4, "CYCLE DELTA:");
+	vis_print_line(vm->vis->graphics->data, 5, 22, CYCLE_DELTA);
+	mvwprintw(vm->vis->graphics->data, 6, 4, "LIVE REPORTS:");
+	vis_print_line(vm->vis->graphics->data, 6, 22, vm->live_count);
+	mvwprintw(vm->vis->graphics->data, 7, 4, "NBR LIVE:");
+	vis_print_line(vm->vis->graphics->data, 7, 22, NBR_LIVE);
+	mvwprintw(vm->vis->graphics->data, 8, 4, "WINNING PLAYER:");
 	vis_color(vm);
-	mvwprintw(vm->vis->graphics->data, 6, 22, str);
-	ft_strdel(&str);
-	vis_print_players(vm);
+	vis_print_line(vm->vis->graphics->data, 8, 22, vm->last_live);
+	vis_print_players(vm, 10);
 	wrefresh(vm->vis->graphics->data);
 	return (0);
 }
